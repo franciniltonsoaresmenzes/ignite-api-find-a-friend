74066afe09d4d0f33c5ce4e5b730b661fdd2dbcf
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-05-29 20:46:08 -0300

    test:add test to verify if it is not possible to register an organization with the same email twice

--- src/use-case/register.spec.ts
+++ src/use-case/register.spec.ts
@@ -1,12 +1,19 @@
 import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository'
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { RegisterUsaCase } from './register'
+import { compare } from 'bcryptjs'
+import { OrgAlreadyExists } from './error/org-already-exist'
+
+let repo: InMemoryOrgsRepository
+let sut: RegisterUsaCase
 
 describe('Register Use Case', () => {
-  it('should to be able register', async () => {
-    const repo = new InMemoryOrgsRepository()
-    const sut = new RegisterUsaCase(repo)
+  beforeEach(() => {
+    repo = new InMemoryOrgsRepository()
+    sut = new RegisterUsaCase(repo)
+  })
 
+  it('should to be able register', async () => {
     const { org } = await sut.execute({
       name: 'jhon doe',
       email: 'jhon@email.com',
@@ -16,6 +23,31 @@ describe('Register Use Case', () => {
       whatsap_number: '949917856',
     })
 
+    const isPasswordCorrectlyHashed = await compare('123456', org.password_hash)
+
     expect(org.id).toEqual(expect.any(String))
+    expect(isPasswordCorrectlyHashed).toBeTruthy()
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    await sut.execute({
+      name: 'Francinilton',
+      email: 'jhon@email.com',
+      password: '123456',
+      address: 'rua exemplo',
+      cep: '123456',
+      whatsap_number: '949917856',
+    })
+
+    await expect(() =>
+      sut.execute({
+        name: 'jhon doe',
+        email: 'jhon@email.com',
+        password: '123456',
+        address: 'rua exemplo',
+        cep: '123456',
+        whatsap_number: '949917856',
+      }),
+    ).rejects.toBeInstanceOf(OrgAlreadyExists)
   })
 })
