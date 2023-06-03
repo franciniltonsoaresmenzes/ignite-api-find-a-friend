873e40cc292a1598416bb26385487639b5b7c356
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-06-02 22:00:26 -0300

    test: router register

---
+++ src/http/controller/orgs/register.spec.ts
@@ -0,0 +1,26 @@
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Register e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should to be able register', async () => {
+    const response = await request(app.server).post('/orgs').send({
+      name: 'Francinilton Soares',
+      email: 'fran@gmail.com',
+      password: '123456',
+      address: 'Ourilãndia do Norte',
+      cep: '0000.0000',
+      whatsap_number: '94 99145-9865',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
